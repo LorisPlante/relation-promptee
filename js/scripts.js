a867b56b3df628e4cf80528265c15e5334eaac00
Loris Planté <loris.plante@hotmail.fr>
2023-11-29 21:25:11 +0100

    swiper zoom sur DONE:fire::rocket:

--- js/scripts.js
+++ js/scripts.js
@@ -1,61 +1,7 @@
-const chatBot = document.getElementById("chatContainer");
-const chatBox = document.getElementById("chatBox");
-const userInput = document.getElementById("userInput");
-
 let prevScrollPos = window.scrollY || document.documentElement.scrollTop;
 
 const parent = document.getElementById("parent");
 const follower = document.getElementById("follower");
-
-// ---------- ChatBot ----------
-
-function openChatBot() {
-  chatBot.classList.toggle("right-2");
-  chatBot.classList.toggle("right-[-500px]");
-}
-
-function closeChatBot() {
-  chatBot.classList.remove("right-2");
-  chatBot.classList.add("right-[-500px]");
-}
-
-function sendMessage() {
-  const userMessage = userInput.value;
-  if (userMessage.trim() === "") {
-    return;
-  }
-
-  appendMessage("user", userMessage);
-  // You can add your chatbot logic here and get a response
-  const botResponse = getBotResponse(userMessage);
-
-  appendBotMessage("bot", botResponse);
-  userInput.value = "";
-}
-
-function appendMessage(sender, message) {
-  const messageElement = document.createElement("div");
-  messageElement.classList.add(sender === "user" ? "user-message" : "bot-message");
-  messageElement.classList.add("p-2", "bg-violet", "text-white", "rounded-md", "w-10/12", "ml-auto", "mr-0", "my-2");
-
-  messageElement.textContent = message;
-  chatBox.appendChild(messageElement);
-  chatBox.scrollTop = chatBox.scrollHeight; // Scroll to the bottom
-}
-
-function appendBotMessage(sender, message) {
-  const messageElement = document.createElement("div");
-  messageElement.classList.add(sender === "bot" ? "user-message" : "bot-message");
-  messageElement.classList.add("p-2", "bg-gray-200", "text-black", "rounded-md", "w-10/12", "mr-auto", "ml-0");
-  messageElement.textContent = message;
-  chatBox.appendChild(messageElement);
-  chatBox.scrollTop = chatBox.scrollHeight; // Scroll to the bottom
-}
-
-function getBotResponse(userMessage) {
-  // Simple example: Echo the user's message as the bot's response
-  return "You said: " + userMessage;
-}
 
 // ---------- scroll down header desktop ----------
 
